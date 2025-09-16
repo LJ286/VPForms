d56141711cc931852827379c925f88455bf17144
LJ286 <joshuatulangi.main@gmail.com>
2025-09-16 13:40:54 +0800

    Add files via upload

---
+++ WindowsFormsApp (Latihan antar Form)/WindowsFormsApp (Latihan antar Form)/Form2.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp__Latihan_antar_Form_
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Logout_click(object sender, EventArgs e)
+        {
+            Form1 form1 = new Form1();
+            this.Hide();
+            form1.Show();
+        }
+    }
+}
